e7691064c906fc4b7410d9fb747de7ea1c7b50f9
Miquel Martín <miquel.martin@bitgenoma.com>
2021-05-25 22:01:41 +0200

    fix certs

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,13 @@
 import { Config } from '@stencil/core';
 import { readFileSync } from 'fs';
 
+// Generate self signed certificate:
+// openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+// With password:
+// openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+
 export const config: Config = {
-  namespace: 'webcam-component',
+  namespace: 'camera-component',
   outputTargets: [
     {
       type: 'dist',
@@ -22,8 +27,13 @@ export const config: Config = {
   devServer: {
     reloadStrategy: 'pageReload',
     https: {
+        // Generate self signed certificate:
+        // openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+        // With password:
+        // openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+        // chrome://flags/#allow-insecure-localhost --> enable
         cert: readFileSync('./ssl/cert.pem', 'utf-8'),
-        key: readFileSync('./ssl/key-open.pem', 'utf-8'),
+        key: readFileSync('./ssl/key.pem', 'utf-8'),
     },
   },
 };
